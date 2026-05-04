b6c106178c48c08d3f43a72db0dc68916854c0ea
David <david11jv@gmail.com>
2026-05-04 16:07:30 +0200

    Add `TransactionScheduler` for scheduled transaction processing
    - Introduced `TransactionScheduler` to read and process transactions from a CSV file periodically.
    - Added sample `transactions.csv` file with test data.
    - Enabled logging for transaction reading and processing.

---
+++ src/main/kotlin/com/davinchicoder/ledgerkt/transaction/infrastructure/scheduler/TransactionScheduler.kt
@@ -0,0 +1,40 @@
+package com.davinchicoder.ledgerkt.transaction.infrastructure.scheduler
+
+import com.davinchicoder.ledgerkt.common.logger
+import com.davinchicoder.ledgerkt.transaction.domain.Transaction
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.io.File
+import java.time.Instant
+import java.util.*
+
+@Component
+class TransactionScheduler {
+    companion object {
+        private val log = logger<TransactionScheduler>()
+    }
+
+    @Scheduled(cron = "* * 0 * * *")
+    fun readTransaction() {
+        log.info("Reading transaction")
+
+        val transactions = File("src/main/resources/transactions.csv")
+            .useLines { lines ->
+                lines.drop(1)
+                    .map { line ->
+                        val parts = line.split(",")
+                        Transaction(
+                            fromAccount = UUID.fromString(parts[0]),
+                            toAccount = UUID.fromString(parts[1]),
+                            amount = parts[2].toBigDecimal(),
+                            id = UUID.randomUUID(),
+                            idempotencyKey = UUID.randomUUID(),
+                            createdAt = Instant.now()
+                        )
+                    }.toList()
+            }
+
+        log.info("Final transactions: $transactions")
+    }
+
+}
